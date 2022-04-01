c426545d386c248f015b3e0bf31ee6c61e1069d6
Sebastián Fernández <sebasfernandezgar@gmail.com>
2022-04-01 20:11:01 +0100

    Apple Scroll Animation

---
+++ app.js
@@ -0,0 +1,37 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+
+//END SECTION
+const section= document.querySelector('section');
+const end = section.querySelector('h1');
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+const scene = new ScrollMagic.Scene({
+    duration: 2000,
+    triggerElemnt: intro,
+    triggerHook: 0
+})
+//    .addIndicators()
+    .setPin(intro)
+    .addTo(controller);
+
+//Video Animation
+let accelamount = 0.9;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+    scrollpos = e.scrollPos / 300;
+    console.log(scrollpos);
+});
+
+setInterval(()=>{
+    delay += (scrollpos - delay) * accelamount;
+    console.log(scrollpos, delay);
+
+    video.currentTime = delay;
+}, 33.3);
